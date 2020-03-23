e8b181f541ad497f5ea4a680d8d6a7c88ca68fac
Vince Linise <32978709+ecnivtwelve@users.noreply.github.com>
2020-03-23 22:46:11 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_daisy.mk
+    $(LOCAL_DIR)/pa_daisy.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_daisy-user \
-    aosp_daisy-userdebug \
-    aosp_daisy-eng
+    pa_daisy-user \
+    pa_daisy-userdebug \
+    pa_daisy-eng
