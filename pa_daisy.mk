f84ea8e11c29875b891cb79074951ea9480cd490
Vince Linise <32978709+ecnivtwelve@users.noreply.github.com>
2020-03-23 22:59:03 +0100

    Update pa_daisy.mk

--- pa_daisy.mk
+++ pa_daisy.mk
@@ -21,12 +21,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from daisy device
 $(call inherit-product, device/xiaomi/daisy/device.mk)
 
-# Inherit some common AEX stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common PA stuff.
+$(call inherit-product, vendor/pa/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := daisy
-PRODUCT_NAME := aosp_daisy
+PRODUCT_NAME := pa_daisy
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A2 Lite
